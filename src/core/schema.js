bc142af64a396474bde818d671daf4c5872cfbcf
chb.wang <chb.wang@hnair.com>
2019-03-04 11:36:38 +0800

    default value nested error

--- src/core/schema.js
+++ src/core/schema.js
@@ -141,8 +141,8 @@ function defaultValue (schema, key, model) {
   if (type === 'object') {
     model[key] = {}
 
-    _.each(schema.properties, function (val, key) {
-      defaultValue(val, key, model[key])
+    _.each(schema.properties, function (val, _key) {
+      defaultValue(val, _key, model[key])
     })
   } else if (type === 'array') {
     model[key] = []
